5981a83a0b3524b27b0ee5bf882b1a718f886426
ItsSilva <juanse909@hotmail.com>
2025-08-09 22:41:18 -0500

    feat: LatestUpdates component update

--- my-react-app/src/components/LatestUpdates/LatestUpdates.jsx
+++ my-react-app/src/components/LatestUpdates/LatestUpdates.jsx
@@ -1,24 +1,22 @@
 import './LatestUpdates.css';
-
 import LatestUpdatesData from '../../data/LatestUpdatesData';
-
-import Button from '../button/Button';
+import Button from '../Button/Button';
 import LatestUpdatesCard from '../LatestUpdatesCard/LatestUpdatesCard';
 
 const LatestUpdates = () => {
-	return (
-		<section className="latest-updates">
-			<div className="latest-updates-header">
-			<h2>Latest Updates</h2>
-			<Button label={'More'}/>
-			</div>
-			<div className="latest-updates-list">
-				{LatestUpdatesData.map(update => (
-					<LatestUpdatesCard key={update.id} update={update} />
-				))}
-			</div>
-		</section>
-	);
+    return (
+        <section className="latest-updates">
+            <div className="latest-updates-header">
+                <h2>Latest Updates</h2>
+                <Button label={'More'} />
+            </div>
+            <div className="latest-updates-list">
+                {LatestUpdatesData.map(update => (
+                    <LatestUpdatesCard key={update.id} update={update} />
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default LatestUpdates;
