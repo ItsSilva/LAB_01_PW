8b130f498bb6d329c604113bd6c11818fee67a5e
ItsSilva <juanse909@hotmail.com>
2025-08-09 21:51:21 -0500

    feat: Header Update

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -2,23 +2,53 @@ import './Header.css';
 import ButtonHeader from '../ButtonHeader/ButtonHeader';
 
 const Header = () => {
-	return (
-		<header className='custom-header'>
-            <img src="../../../src/assets/header.jpg" alt="Honkai: Star Rail" />
-            <div>
-            <img src='../../../src/assets/appicon.png' alt='Logo' className='logo' />
-			<p>Honkai: Star Rail</p>
-            </div>
+  return (
+    <header className='custom-header'>
+      <img 
+        src="../../../src/assets/header.jpg" 
+        alt="Honkai: Star Rail Background" 
+        className="header-background"
+      />
+      
+      <div className="header-content">
+        <div className="game-info">
+          <img 
+            src='../../../src/assets/appicon.png' 
+            alt='Honkai: Star Rail Logo' 
+            className='game-logo' 
+          />
+          <h1 className="game-title">Honkai: Star Rail</h1>
+        </div>
 
-            <div>
-            <p>Honkai: Star Rail V3.4 "For the Sun is Set to Die" Is Now Online!</p>
-            </div>
+        <h1 className="main-title">
+          Honkai: Star Rail V3.4<br />
+          "For the Sun is Set to<br />
+          Die" Is Now Online!
+        </h1>
+        <div className="cta-section">
+          <ButtonHeader />
+        </div>
+      </div>
 
-            <div>
-			<ButtonHeader />
-            </div>
-		</header>
-	);
+      <div className="bottom-thumbnails">
+        <img 
+          src="../../../src/assets/header.jpg" 
+          alt="Thumbnail 1" 
+          className="thumbnail active"
+        />
+        <img 
+          src="../../../src/assets/header1.jpg" 
+          alt="Thumbnail 2" 
+          className="thumbnail"
+        />
+        <img 
+          src="../../../src/assets/header2.jpg" 
+          alt="Thumbnail 3" 
+          className="thumbnail"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
