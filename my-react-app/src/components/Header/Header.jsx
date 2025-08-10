c8472c0e913c749a612362c5572fdb83b3a2a34b
ItsSilva <juanse909@hotmail.com>
2025-08-10 11:24:56 -0500

    feat: Header carrusel working

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,55 +1,64 @@
 import './Header.css';
 import ButtonHeader from '../ButtonHeader/ButtonHeader';
-
 import HeaderData from '../../data/HeaderData';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Auto-play functionality
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % HeaderData.length);
+    }, 5000); // Cambia cada 5 segundos
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Handle thumbnail click
+  const handleThumbnailClick = (index) => {
+    setCurrentSlide(index);
+  };
+
+  const currentData = HeaderData[currentSlide];
+
   return (
     <header className='custom-header'>
-      {HeaderData.map(data => (
-        <div key={data.id} className="header-item">
-         <img 
-        src={data.image} 
-        alt={data.title} 
+      <img 
+        src={currentData.image} 
+        alt={currentData.title} 
         className="header-background"
       />
       
       <div className="header-content">
         <div className="game-info">
           <img 
-            src={data.icon} 
-            alt={data.title} 
+            src={currentData.icon} 
+            alt={currentData.title} 
             className='game-logo' 
           />
-          <h1 className="game-title">{data.title}</h1>
+          <h1 className="game-title">{currentData.title}</h1>
         </div>
 
         <h1 className="main-title">
-          {data.description}
+          {currentData.description}
         </h1>
+        
         <div className="cta-section">
           <ButtonHeader />
         </div>
       </div>
-        </div>
-      ))}
 
       <div className="bottom-thumbnails">
-        <img 
-          src="../../../src/assets/header.jpg" 
-          alt="Thumbnail 1" 
-          className="thumbnail active"
-        />
-        <img 
-          src="../../../src/assets/header1.jpg" 
-          alt="Thumbnail 2" 
-          className="thumbnail"
-        />
-        <img 
-          src="../../../src/assets/header2.jpg" 
-          alt="Thumbnail 3" 
-          className="thumbnail"
-        />
+        {HeaderData.map((data, index) => (
+          <img 
+            key={data.id}
+            src={data.image} 
+            alt={`Thumbnail ${index + 1}`} 
+            className={`thumbnail ${index === currentSlide ? 'active' : ''}`}
+            onClick={() => handleThumbnailClick(index)}
+          />
+        ))}
       </div>
     </header>
   );
