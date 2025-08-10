8ef4ce2b1933d982966542adeaa8d0a8fc586e87
ItsSilva <juanse909@hotmail.com>
2025-08-09 21:51:29 -0500

    feat: Navbar update

--- my-react-app/src/components/Navbar/Navbar.jsx
+++ my-react-app/src/components/Navbar/Navbar.jsx
@@ -1,51 +1,41 @@
 import './Navbar.css'
 import NavbarButton from '../NavbarButton/NavbarButton.jsx'
-import { useState } from 'react'
 
-function Navbar() {
-  const [activeTab, setActiveTab] = useState('Home');
-
-  const handleTabClick = (tabName) => {
-    setActiveTab(tabName);
-    console.log(`${tabName} clicked`);
-  };
-
+function Navbar({ activeTab, onTabClick }) {
   return (
     <>
-      <nav className='custom-navbar'>
-        <div className='navbar-logo'>
-          <a href="#">
-            <img src="../src/assets/logo.png" alt="HoYoverse Logo" />
-          </a>
-        </div>
-        <div className='navbar-links'>
-          <NavbarButton 
-            label="Home" 
-            onClick={() => handleTabClick('Home')}
-            isActive={activeTab === 'Home'}
-          />
-          <NavbarButton 
-            label="News" 
-            onClick={() => handleTabClick('News')}
-            isActive={activeTab === 'News'}
-          />
-          <NavbarButton 
-            label="About Us" 
-            onClick={() => handleTabClick('About Us')}
-            isActive={activeTab === 'About Us'}
-          />
-          <NavbarButton 
-            label="Careers" 
-            onClick={() => handleTabClick('Careers')}
-            isActive={activeTab === 'Careers'}
-          />
-          <NavbarButton 
-            label="Help Center" 
-            onClick={() => handleTabClick('Help Center')}
-            isActive={activeTab === 'Help Center'}
-          />
-        </div>
-      </nav>
+      <div className='navbar-logo'>
+        <a href="#">
+          <img src="../src/assets/logo.png" alt="HoYoverse Logo" />
+        </a>
+      </div>
+      <div className='navbar-links'>
+        <NavbarButton 
+          label="Home" 
+          onClick={() => onTabClick('Home')}
+          isActive={activeTab === 'Home'}
+        />
+        <NavbarButton 
+          label="News" 
+          onClick={() => onTabClick('News')}
+          isActive={activeTab === 'News'}
+        />
+        <NavbarButton 
+          label="About Us" 
+          onClick={() => onTabClick('About Us')}
+          isActive={activeTab === 'About Us'}
+        />
+        <NavbarButton 
+          label="Careers" 
+          onClick={() => onTabClick('Careers')}
+          isActive={activeTab === 'Careers'}
+        />
+        <NavbarButton 
+          label="Help Center" 
+          onClick={() => onTabClick('Help Center')}
+          isActive={activeTab === 'Help Center'}
+        />
+      </div>
     </>
   )
 }
