4e01276bc236d52f49be872e6a1faba6512c9036
ItsSilva <juanse909@hotmail.com>
2025-08-10 10:55:01 -0500

    feat: Footer component added

--- my-react-app/src/components/Footer/Footer.jsx
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -1,27 +1,28 @@
 import './Footer.css'
-
 import { BiWorld } from "react-icons/bi";
 
 const Footer = () => {
     return (
         <footer>
-        <div className="footer-info">
-            <p>Home</p>
-            <p>News</p>
-            <p>About Us</p>
-            <p>Careers</p>
-            <p>Business Affairs</p>
-            <p>Privacy Policy</p>
-            <p>Terms of Service</p>
-            <p>Help Center</p>
-        </div>
-        <div className="footer-logo">
-            <img src="../../../src/assets/logo.png" alt="Logo" />
-            <p>Copyright © COGNOSPHERE. All Rights Reserved.</p>
-        </div>
-        <div className='world-icon'>
-            <BiWorld />
-        </div>
+            <div className="footer-content">
+                <div className="footer-info">
+                    <p>Home</p>
+                    <p>News</p>
+                    <p>About Us</p>
+                    <p>Careers</p>
+                    <p>Business Affairs</p>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
+                    <p>Help Center</p>
+                </div>
+                
+                <div className="footer-bottom">
+                    <div className="footer-logo">
+                        <img src="../../../src/assets/logo.png" alt="HoYoverse Logo" />
+                    </div>
+                    <p className="footer-copyright">Copyright © COGNOSPHERE. All Rights Reserved.</p>
+                </div>
+            </div>
         </footer>
     )
 }
