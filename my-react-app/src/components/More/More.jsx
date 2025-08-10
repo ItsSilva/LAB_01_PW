a716f2342380f74d304674300cf2ada39dc195a6
ItsSilva <juanse909@hotmail.com>
2025-08-10 10:02:49 -0500

    feat: More component update for css improve

--- my-react-app/src/components/More/More.jsx
+++ my-react-app/src/components/More/More.jsx
@@ -1,13 +1,15 @@
 import './More.css'
-
 import MoreCard from '../MoreCard/MoreCard';
 
 const More = () => {
     return (
-        <section>
-            <img src="../../../src/assets/BackImage/backimage1.png" alt="Background Galaxy Image" />
+        <section className="more-section">
+            <img 
+                src="../../../src/assets/BackImage/backimage1.png" 
+                alt="Background Galaxy Image" 
+                className="more-background"
+            />
             <div className="more-container">
-                <h2>More Information</h2>
                 <MoreCard title="About Us" />
                 <MoreCard title="Careers" />
             </div>
