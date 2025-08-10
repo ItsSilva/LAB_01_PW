978da1a39fa3a243cb2fce12121f5767d4a66431
ItsSilva <juanse909@hotmail.com>
2025-08-09 23:22:52 -0500

    feat: OurProductsData added

---
+++ my-react-app/src/data/OurProductsData.jsx
@@ -0,0 +1,46 @@
+const OurProductsData = [
+    {
+        id: 1,
+        image: '../../src/assets/OurProductsDataImages/banner1.jpg',
+        icon: '../../src/assets/OurProductsDataImages/icon1.png',
+        title: 'Zenless Zone Zero',
+        description: 'Welcome to New Eridu!',
+    },
+    {
+        id: 2,
+        image: '../../src/assets/OurProductsDataImages/banner2.jpeg',
+        icon: '../../src/assets/OurProductsDataImages/icon2.jpeg',
+        title: 'Genshin Impact',
+        description: 'Step into a vast magical world of adventure',
+    },
+    {
+        id: 3,
+        image: '../../src/assets/OurProductsDataImages/banner3.jpeg',
+        icon: '../../src/assets/OurProductsDataImages/icon3.jpeg',
+        title: 'Honkai Impact 3rd',
+        description: 'Fight for all that is beautiful in the world',
+    },
+    {
+        id: 4,
+        image: '../../src/assets/OurProductsDataImages/banner4.jpeg',
+        icon: '../../src/assets/OurProductsDataImages/icon4.jpeg',
+        title: 'Honkai: Star Rail',
+        description: 'May this journey lead us starward.',
+    },
+    {
+        id: 5,
+        image: '../../src/assets/OurProductsDataImages/banner5.jpeg',
+        icon: '../../src/assets/OurProductsDataImages/icon5.jpeg',
+        title: 'HoYoLAB',
+        description: 'HoYoLAB is a community forum, featuring official information on our popular games, a variety of fan art, useful tools, exclusive perks, player anecdotes, and more.',
+    },
+    {
+        id: 6,
+        image: '../../src/assets/OurProductsDataImages/banner6.jpg',
+        icon: '../../src/assets/OurProductsDataImages/icon6.jpg',
+        title: 'Tears of Themis',
+        description: 'Embark now on an adventure of romance and wits!',
+    }
+];
+
+export default OurProductsData;
