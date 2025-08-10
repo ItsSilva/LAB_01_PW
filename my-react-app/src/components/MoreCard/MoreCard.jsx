33c82e97d3bca63008a05c125444424e01ce6eb7
ItsSilva <juanse909@hotmail.com>
2025-08-10 10:02:54 -0500

    feat: MoreCard component update for css improve

--- my-react-app/src/components/MoreCard/MoreCard.jsx
+++ my-react-app/src/components/MoreCard/MoreCard.jsx
@@ -4,12 +4,15 @@ import { FaArrowRight } from "react-icons/fa6";
 const MoreCard = ({ title }) => {
     return (
         <div className='more-card'>
-            <h3>{title}</h3>
-            <footer>
-                <button>
+            <div className="more-card-title-section">
+                <h3>{title}</h3>
+                <div className="more-card-line"></div>
+            </div>
+            <div className="more-card-footer">
+                <button className="more-card-button">
                     <FaArrowRight />
                 </button>
-            </footer>
+            </div>
         </div>
     )
 }
