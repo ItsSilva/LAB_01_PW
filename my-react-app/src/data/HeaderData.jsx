3c9a168713a3926f1307db0b6c20f4e6c395c2ec
ItsSilva <juanse909@hotmail.com>
2025-08-10 11:05:53 -0500

    feat: Data for header carrusel added

---
+++ my-react-app/src/data/HeaderData.jsx
@@ -0,0 +1,24 @@
+const HeaderData = [
+{
+    id: 1,
+    icon: '../../src/assets/HeaderDataImages/icon1.png',
+    title: 'Honkai: Star Rail',
+    description: 'Honkai: Star Rail V3.4 "For the Sun is Set to Die" Is Now Online!',
+    image: '../../src/assets/HeaderDataImages/background1.jpg',
+},
+{
+    id: 2,
+    icon: '../../src/assets/HeaderDataImages/icon2.jpeg',
+    title: 'Genshin Impact',
+    description: 'Latest Updates on Version 5.8 "Sunspray Summer Resort"',
+    image: '../../src/assets/HeaderDataImages/background2.jpg',
+},
+{
+    id: 3,
+    icon: '../../src/assets/HeaderDataImages/icon3.jpeg',
+    title: 'Honkai Impact 3rd',
+    description: 'v8.3 \n\'[Flickers of a Spacetime Warp] \nAvailable Now!',
+    image: '../../src/assets/HeaderDataImages/background3.jpg',
+},
+]
+export default HeaderData;
