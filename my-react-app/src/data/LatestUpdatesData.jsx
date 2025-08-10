aa15972a78f66a0489744dd6b6ff557145429ec7
ItsSilva <juanse909@hotmail.com>
2025-08-09 22:25:01 -0500

    feat: data added for Lates Updates section

---
+++ my-react-app/src/data/LatestUpdatesData.jsx
@@ -0,0 +1,52 @@
+const LatestUpdatesData = [
+	{
+		id: 1,
+		game: 'Honkai Impact 3rd',
+		content: 'Honkai Impact 3rd Launching v7.9 [Stars Derailed] on ',
+        image: '../../src/assets/LatestUpdatesDataImages/image1.jpg',
+	},
+	{
+		id: 2,
+		game: 'Genshin Impact',
+		content: 'Genshin Impact Version 5.2: Enhanced Aerial Abilities in Natlan – Coming November 20',
+		        image: '../../src/assets/LatestUpdatesDataImages/image2.jpeg',
+
+	},
+	{
+		id: 3,
+		game: 'Genshin Impact',
+		content: 'Genshin Impact Version 5.8 Refreshes Your Summer at Easybreeze Holiday Resort in Natlan on July 30!',
+		        image: '../../src/assets/LatestUpdatesDataImages/image3.jpeg',
+
+	},
+    {
+        id: 4,
+        game: '',
+        content: 'Zenless Zone Zero Version 2.1 "The Impending Crash of Waves" Sets Sail for Summer Paradise on July 16',
+                image: '../../src/assets/LatestUpdatesDataImages/image4.jpeg',
+
+    },
+    {
+        id: 5,
+        game: 'HoYoverse',
+        content: 'HoYoverse Announces Gamescom 2025 Lineup',
+                image: '../../src/assets/LatestUpdatesDataImages/image5.jpeg',
+
+    },
+    {
+        id: 6,
+        game: 'Honkai: Star Rail',
+        content: 'Honkai: Star Rail Version 3.4 "For the Sun is Set to Die" Will Land on July 2',
+                image: '../../src/assets/LatestUpdatesDataImages/image6.jpeg',
+
+    },
+    {
+        id: 7,
+        game: 'Genshin Impact',
+        content: 'Genshin Impact Version 5.7 Welcomes Skirk, the Sinner\'s Pupil and Fool\'s Master, on June 18th',
+                image: '../../src/assets/LatestUpdatesDataImages/image7.jpeg',
+
+    }
+];
+
+export default LatestUpdatesData;
